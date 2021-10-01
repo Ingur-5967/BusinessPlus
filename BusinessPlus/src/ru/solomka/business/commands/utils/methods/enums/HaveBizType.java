0f56ec77a3f5f8a98cf47dbee484dd269cde53ca
Serega <76917656+sss606@users.noreply.github.com>
2021-10-01 21:11:28 +0300

    Delete BusinessPlus directory

--- BusinessPlus/src/ru/solomka/business/commands/utils/methods/enums/HaveBizType.java
+++
@@ -1,5 +0,0 @@
-package ru.solomka.business.commands.utils.methods.enums;
-
-public enum HaveBizType {
-    ALL,SIZE
-}
--- BusinessPlus/src/ru/solomka/business/menu/enums/InventoryType.java
+++
@@ -1,5 +0,0 @@
-package ru.solomka.business.menu.enums;
-
-public enum InventoryType {
-    MAIN, CHANGE_CLASS, UPDATE, LOW_CLASS, MEDIUM_CLASS, HIGH_CLASS, DEVELOP_BIZ
-}
