d8a3fdb3997f50153303ccd93b035252783df055
Serega <76917656+sss606@users.noreply.github.com>
2021-10-02 19:01:44 +0300

    Delete src directory

--- src/ru/solomka/business/commands/utils/methods/enums/BusinessClassType.java
+++
@@ -1,7 +0,0 @@
-package ru.solomka.business.commands.utils.methods.enums;
-
-public enum BusinessClassType {
-    LOW,
-    MEDIUM,
-    HIGH
-}
--- src/ru/solomka/business/menu/events/DevelopBizMenu.java
+++
@@ -1,9 +0,0 @@
-package ru.solomka.business.menu.events;
-
-import org.bukkit.event.Listener;
-
-public class DevelopBizMenu implements Listener {
-
-    // TODO 01/10/21
-
-}
